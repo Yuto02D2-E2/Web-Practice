2cee0bf7dd33c2feb893f17b7db59d4179350404
Yuto02D2-E2 <d.yuto.k.b@gmail.com>
2021-09-22 02:36:38 +0900

    finish MDN/LearningArea/JS/FirstSteps/

--- MDN/JS/index.js
+++ MDN/JS/index.js
@@ -5,7 +5,11 @@ write on 2021/09/21
 
 
 /* MDNチュートリアルの数値あてゲーム */
-let random_num = Math.floor(Math.random() * 100) + 1; // constとletの違いが分からん
+
+// jsの変数定義ではvar/let/constの三種類が使えるが，let/constのみを使用する
+// varは古い言語使用なので，letの方が安全．定数にはconstを使用する
+// 全部書き終わってからコメント書いてるから今更やけど，JSでは変数名にlowerCamelCaseが使われるらしい
+let random_num = Math.floor(Math.random() * 100) + 1;
 
 const guess = document.querySelector(".guess");
 const last_res = document.querySelector(".last_res");
@@ -19,7 +23,7 @@ let reset_btn;
 
 // jsファイルがこの場所まで読み込まれた時点で入力フォーム(field)にフォーカスする
 // ちなみに，focus()はhtmlのinputタグに対応するメソッド
-field.focus();
+// field.focus();
 
 // alert("guessing game start!!!");
 
@@ -81,9 +85,67 @@ function reset_game() {
 }
 
 
-/* その他 */
+/* canvas */
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, 100, 200); // (x, y, w, h)
+
+
+/*  alert prompt */
 let greeting = document.querySelector(".greeting");
 greeting.onclick = function () {
     let user_name = prompt("what your name: ");
-    alert("hello ${user_name}, have a nice day!");
+    // alert("hello " + user_name + ", have a nice day!");
+
+    // プレースホルダー${}で文字列に変数を埋め込める
+    // 注意：テンプレートリテラルを使用する場合はダブル/シングルクォートをバッククォートに変更しなければならない
+    alert(`hello ${user_name}, have a nice day!`);
+}
+
+
+/* バカ話ジェネレーター */
+const customName = document.getElementById('customname');
+customName.focus();
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", clear_story);
+function clear_story() {
+    story.style.visibility = "hidden"; // visibility := visible/hidden
+}
+
+function randomValueFromArray(array) {
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
+}
+
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const insertX = "Willy the Goblin,Big Daddy,Father Christmas".split(",");
+const insertY = "the soup kitchen,Disneyland,the White House".split(",");
+const insertZ = "spontaneously combusted,melted into a puddle on the sidewalk,turned into a slug and crawled away".split(",");
+
+randomize.addEventListener("click", result);
+function result() {
+    let newStory = storyText; // Rustだと所有権の問題でこんなことしたらダメだけど，JSならできちゃう
+    let xItem = randomValueFromArray(insertX); // ここの処理汚いな．xItem-zItemを一つの配列にしてloopで回したい
+    newStory = newStory.replace(/:insertx:/g, xItem);
+    let yItem = randomValueFromArray(insertY);
+    newStory = newStory.replace(/:inserty:/g, yItem);
+    let zItem = randomValueFromArray(insertZ);
+    newStory = newStory.replace(/:insertz:/g, zItem);
+
+    if (customName.value !== "") {
+        let name = customName.value;
+        newStory = newStory.replace(/Bob/g, name);  // replaceは一度に一か所しか置換できないので正規表現を利用する
+    }
+    if (document.getElementById("uk").checked) {
+        let weight = Math.round(300 * 0.071429) + " stone";
+        newStory = newStory.replace(/300 pounds/g, weight);
+        let temperature = Math.round((94 - 32) / 1.8) + " centigrade";
+        newStory = newStory.replace(/94 fahrenheit/g, temperature);
+    }
+    story.textContent = newStory;
+    story.style.visibility = "visible";
 }
---
+++ MDN/README.md
@@ -0,0 +1,4 @@
+
+## References
+- [mdn learning area ja](https://developer.mozilla.org/ja/docs/Learn)
+- [mdn learning area github repo](https://github.com/mdn/learning-area)
